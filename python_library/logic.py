9dbb25d1600ca9254be166a9f7ab0e6dc8ce2b57
EC2 Default User <ec2-user@ip-172-31-44-196.ec2.internal>
2022-07-17 05:27:21 +0000

    removed csv files

--- python_library/logic.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-filename="/home/ec2-user/environment/glassdoor-app/python_library/tech-companies.csv"
-
-# opening the file using "with"
-# statement
-with open(filename, 'r') as data:
-	for line in csv.DictReader(data):
-		print(line)
