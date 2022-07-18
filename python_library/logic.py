acafbbc3b6f25dd45125dd0cfef4f10b48f4245d
EC2 Default User <ec2-user@ip-172-31-44-196.ec2.internal>
2022-07-18 00:02:01 +0000

    wrote app

--- python_library/logic.py
+++ python_library/logic.py
@@ -1,15 +1,3 @@
-import json
- 
-# Opening JSON file
-def convert():
-    with open('/home/ec2-user/environment/glassdoor-app/python_library/tech-companies.json') as json_file:
-        data = json.load(json_file)
-        for dic in data:
-            url = str(dic['Name'])
-            url2 = url.lower()
-            url3 = url2.replace(" ", "")
-            url4 = url3 + '.com/careers'
-            li = list(url4.split(" "))
-            print(li)
+from tech-companies-json import company_list
 
-print(convert())
+print(type(company_list))
